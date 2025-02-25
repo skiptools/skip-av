72a67674345ef40bdb5d7b084e21d3c5899c95e5
Marc Prud'hommeaux <marc@skip.tools>
2025-02-25 15:38:06 -0500

    Update copyright and license headers

--- Tests/SkipAVTests/XCSkipTests.swift
+++ Tests/SkipAVTests/XCSkipTests.swift
@@ -1,7 +1,4 @@
-// This is free software: you can redistribute and/or modify it
-// under the terms of the GNU Lesser General Public License 3.0
-// as published by the Free Software Foundation https://fsf.org
-
+// SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
 #if os(macOS) // Skip transpiled tests only run on macOS targets
 import SkipTest
 
